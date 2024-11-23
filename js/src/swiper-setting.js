dfdc7384d84533d19beb305dd484ed8d798cb8a6
Dima <dimak119988@gmail.com>
2024-11-23 02:13:51 +0200

    Version 2.0 Mobile + Desktop

--- js/src/swiper-setting.js
+++ js/src/swiper-setting.js
@@ -1,17 +1,34 @@
 const hakkımızdaSwiper = new Swiper(".hakkımızda__swiper", {
   slidesPerView: "auto",
   spaceBetween: 15,
+
+  breakpoints: {
+    1440: {
+      spaceBetween: 0,
+      allowTouchMove: false,
+    }
+  }
 });
 
 const sıcakOrtamımızSwiper = new Swiper(".sıcak-ortamımız__swiper", {
   slidesPerView: "auto",
   spaceBetween: 24,
   loop: true,
+  centeredSlides: true,
 
   autoplay: {
     delay: 3000,
     disableOnInteraction: false,
   },
+
+  breakpoints: {
+    1440: {
+      slidesPerView: 2,
+      spaceBetween: 0,
+      loop: false,
+      centeredSlides: false,
+    }
+  }
 });
 
 const müşteriYorumlarıSwiper = new Swiper(".müşteri-yorumları__swiper", {
@@ -24,6 +41,14 @@ const müşteriYorumlarıSwiper = new Swiper(".müşteri-yorumları__swiper", {
     delay: 3000,
     disableOnInteraction: false,
   },
+
+  breakpoints: {
+    1440: {
+      centeredSlides: false,
+      spaceBetween: 0,
+      loop: false,
+    }
+  }
 });
 
 
